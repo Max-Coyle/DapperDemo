73dacd0b63a79ea712f226496a8741d8392b545b
Max Coyle <55137324@ad.mmu.ac.uk>
2019-12-11 11:30:30 +0000

    add more untracked files

---
+++ Controllers/CityController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DapperDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public CityController(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository;
+        }
+
+        // GET api/city
+        [HttpGet]
+        public async Task<IEnumerable<City>> Get()
+        {
+            return await _cityRepository.GetListOfCities();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<int> Post([FromBody] City city)
+        {
+            return await _cityRepository.AddCity(city);
+        }
+    }
+}
